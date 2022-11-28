2672db28501981e5ffb787dfc76b3b1aeabfb6ad
Malte <malteaschenbach@gmail.com>
2022-11-28 08:45:44 +0100

    vault backup: 2022-11-28 08:45:44

--- Informatik/collatz/CollatzConjecture.java
+++ Informatik/collatz/CollatzConjecture.java
@@ -1,5 +1,20 @@
 public class CollatzConjecture {
     public static void main(String args[]) {
+        int n = i;
+        System.out.println("Collatz for " + i);
+        do {
+            System.out.println(n);
+            n = collatz(n);
+        } while (n != 1);
+            System.out.println("-------");
+        }
         
+    }
+    
+    public static int collatz(int n) {
+        if (n % 2 == 0)
+            return n / 2;
+        else 
+            return 3 * n + 1;
     }
 } 
